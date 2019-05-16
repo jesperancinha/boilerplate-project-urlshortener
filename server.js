d7434bd4f71c850a768c9b5142d66bea69ebff5e
João F.S.E <jofisaes@hotmail.com>
2019-05-16 20:24:43 +0200

    finished project

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var parser = require('body-parser');
 
 var cors = require('cors');
 
@@ -11,8 +12,17 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
+app.use(parser.urlencoded({extended: false}));
+
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
+
+let Schema = mongoose.Schema;
+let UrlSchema = new Schema({
+  url: String,
+  short: Number
+});
+let Url = mongoose.model('Url', UrlSchema);
 
 app.use(cors());
 
@@ -34,4 +44,45 @@ app.get("/api/hello", function (req, res) {
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
+});
+
+app.get("/api/shorturl/:short", function (req, res) {
+  let short = req.params.short;
+  Url.findOne({short: short}, (err, data)=>{
+    if(err){
+      res.json({"error":"No short url found for given input"});
+    } else if(!data){
+      res.json({"error":"No short url found for given input"});
+    } else {
+      res.redirect(data.url);
+    }
+  });           
+});
+
+app.post("/api/shorturl/new", function (req, res) {
+  let url = req.body.url;
+  if(/^http[s]*:\/\/w{3}\.\w+\.\w+(\/[a-z]+)*$/.test(url)){
+  Url.find({})
+      .sort('-short')
+      .limit(1)
+      .exec((err, higher) => { 
+      var urlObject;
+      if(err || higher.length === 0){
+        urlObject = new Url({ url: url, short: 1});
+      } else {
+        urlObject = new Url({ url: url, short: higher[0].short + 1});
+      }
+
+     Url.findOneAndUpdate(
+        {url: url},
+        {$setOnInsert: urlObject},
+        { upsert: true, new: true, runValidators: true },
+      (err, data)=>{
+            res.json(data);
+      });
+  
+  });
+  }else{
+    res.json({"error":"invalid URL"});
+  }
 });
